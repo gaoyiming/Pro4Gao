1d529fc7e22d2b526bf7f0e1631f5cf7356d711d
yourname <787080436@qq.com>
2016-02-18 16:17:01 +0800

    添加cache

--- app/src/main/java/com/pro4gao/api/AppUtil.java
+++ app/src/main/java/com/pro4gao/api/AppUtil.java
@@ -1,6 +1,8 @@
 package com.pro4gao.api;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 /**
  * Created by gaoyiming on 2016/2/17.
@@ -8,6 +10,12 @@ import android.content.Context;
 public class AppUtil {
 
     public static boolean isNetworkReachable(Context context) {
-        return false;
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo current = cm.getActiveNetworkInfo();
+        if (current == null) {
+            return false;
+        }
+        return (current.isAvailable());
     }
 }
