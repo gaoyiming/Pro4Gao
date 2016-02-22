918c384bd4ef9b81d6782b300c4429cedd651366
yourname <787080436@qq.com>
2016-02-22 11:16:05 +0800

    添加cache

--- app/src/main/java/com/pro4gao/api/APIUtil.java
+++ app/src/main/java/com/pro4gao/api/APIUtil.java
@@ -1,5 +1,6 @@
 package com.pro4gao.api;
 
+import android.nfc.Tag;
 import android.util.Log;
 
 import com.pro4gao.utils.UIUtils;
@@ -12,6 +13,8 @@ import com.squareup.okhttp.Response;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
@@ -21,23 +24,19 @@ import retrofit.RxJavaCallAdapterFactory;
  * Created by gaoyiming on 2016/2/17.
  */
 public class APIUtil {
+    private static final String TAG = "APIUtil";
     private static APIUtil mInstance;
     //此前定义的接口的实例
     private RetroApi Retroapi;
-    public RetroApi getZhixueApi() {
-        return Retroapi;
-    }
     //我们的主域名
     private static final String BASE_URL = "http://124.207.115.50:8011/mobile/";
-
-
     private OkHttpClient client;
+    private Cache cache;
 
     //构造函数
     private APIUtil() {
         //setup cache
         File httpCacheDirectory = new File(UIUtils.getContext().getCacheDir(), "responses");
-
 
         Cache cache = null;
         try {
@@ -45,23 +44,22 @@ public class APIUtil {
         } catch (Exception e) {
             Log.e("OKHttp", "Could not create http cache", e);
         }
-
-
-
         client = new OkHttpClient();
-        client.setCache(cache);
+
         client.interceptors().add(LoggingInterceptor);
         client.networkInterceptors().add(interceptor);
         client.interceptors().add(interceptor);
 
+//add cache to the client
+        client.setCache(cache);
+        //在构造函数中我们要通过实例化RestAdapter拿到我们的ZhixueApi
+        //注: setRequestInterceptor()在这里是为了在请求头中加入设备信息, 方便我们后台的调试
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
-
-
         Retroapi = retrofit.create(RetroApi.class);
     }
 
@@ -73,48 +71,75 @@ public class APIUtil {
         return mInstance;
     }
 
-    private final Interceptor LoggingInterceptor = new Interceptor() {
-        @Override
-        public Response intercept(Chain chain) throws IOException {
-            Request request = chain.request();
-            long t1 = System.nanoTime();
-            Log.e("okhttp", String.format("Sending request %s on %s%n%s", request.url(), chain.connection(), request.headers()));
-            Response response = chain.proceed(request);
-            long t2 = System.nanoTime();
-            Log.e("okhttp", String.format("Received response for %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d, response.headers()));
-            return response;
-        }
-    };
-    private final   Interceptor interceptor = new Interceptor() {
+    public RetroApi getZhixueApi() {
+        return Retroapi;
+    }
+
+    //    private static final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
+//        @Override public Response intercept(Chain chain) throws IOException {
+//            Response originalResponse = chain.proceed(chain.request());
+//            if (AppUtil.isNetworkReachable(UIUtils.getContext())) {
+//                int maxAge = 60; // read from cache for 1 minute
+//                return originalResponse.newBuilder()
+//                        .header("Cache-Control", "public, max-age=" + maxAge)
+//                        .build();
+//            } else {
+//                int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale
+//                return originalResponse.newBuilder()
+//                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+//                        .build();
+//            }
+//        }
+    public Interceptor interceptor = new Interceptor() {
         @Override
         public Response intercept(Chain chain) throws IOException {
             Request request = chain.request();
             if (!AppUtil.isNetworkReachable(UIUtils.getContext())) {
                 request = request.newBuilder()
                         .cacheControl(CacheControl.FORCE_CACHE)
-                        .build();
-                Log.e("OKHttp", "noNet");
+                        .url(BASE_URL).build();
+                Log.e("OKHttp", "meiyouwangluo");
             }
 
             Response response = chain.proceed(request);
             if (AppUtil.isNetworkReachable(UIUtils.getContext())) {
                 int maxAge = 60 * 60; // read from cache for 1 minute
-                return  response.newBuilder()
-                        .header("Cache-Control", "public, max-age=" + maxAge)
+                String cacheControl = request.cacheControl().toString();
+                response.newBuilder()
                         .removeHeader("Pragma")
+                        .header("Cache-Control", "public, max-age=" + maxAge)
                         .build();
             } else {
-                for (int i = 0; i <5 ; i++) {
-                    System.out.print(i);
-                }
                 int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale
-                return  response.newBuilder()
-                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                response.newBuilder()
                         .removeHeader("Pragma")
+                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
                         .build();
             }
-
+            return response;
         }
     };
+    //在这里我们还定义了一个RequestInterceptor, 作用是在请求头中拼入一些信息方便我们后台的调试
+    //否则请求头中就只会出现okhttp 2.2.0的字样(Retrofit默认是直接使用OkhttpClient的)
+//    RequestInterceptor defaultInterceptor = new RequestInterceptor() {
+//        @Override
+//        public void intercept(RequestFacade request) {
+//            request.addHeader("User-Agent", "some code here");
+//        }
+//    };
+    private final Interceptor LoggingInterceptor = new Interceptor() {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+            long t1 = System.nanoTime();
+            Log.e(TAG, String.format("Sending request %s on %s%n%s", request.url(), chain.connection(), request.headers()));
 
+            Response response = chain.proceed(request);
+            long t2 = System.nanoTime();
+            Log.e(TAG, String.format("Received response for %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+
+            return response;
+        }
+
+    };
 }
